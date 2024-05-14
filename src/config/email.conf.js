3300b6caac232cd80f6d67f182f4927f9961c319
Herlan <herlanherbas@gmail.com>
2024-05-14 19:58:28 -0400

    feature(configure email)

---
+++ src/config/email.conf.js
@@ -0,0 +1,11 @@
+module.exports = {
+    emailConfig: {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'herlanz740@gmail.com',
+      pass: 'fsovjbbijfinuyrq'
+    }
+  }
+}
